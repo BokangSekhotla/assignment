8b0e7dd0b51948ba6e50e1b9366f397f1ca12c9a
BokangSekhotla <70110031+BokangSekhotla@users.noreply.github.com>
2022-05-12 10:25:06 -0700

    Update HelloApplication.java

--- src/main/java/com/example/assignment/HelloApplication.java
+++ src/main/java/com/example/assignment/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.assignment;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +14,15 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Media Player");
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleclicked) {
+                if (doubleclicked.getClickCount()==2){
+                    stage.setFullScreen(true);
+                }
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
